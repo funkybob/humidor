9e75cad8e86a0c9e6dd0536a2ee672cb0df1ac77
Curtis Maloney <curtis@tinbrain.net>
2016-06-20 09:57:24 +1000

    Comply with the spec more.  Rename main -> todoList for clarity

--- example/todo.js
+++ example/todo.js
@@ -16,16 +16,19 @@ __.onstart(function () {
     var store = new DOMStore(),
         tmpl = __.get('template#todo-row').innerHTML;
         input = __.get('input.new-todo'),
-        main = __.get('ul.todo-list'),
+        todoList = __.get('ul.todo-list'),
+        main = __.get('.main'),
         footer = __.get('.footer'),
         rowcount = __.get('.todo-count strong');
 
     FILTER = {'': '', 'active': '[data-completed=false]', 'completed': '[data-completed=true]'}
     function render () {
+        var total = store.query({'@type': 'todo'}).length;
         var rows = store.query('[type=todo]' + FILTER[filter.value]);
-        main.innerHTML = rows.map(function (rec) { return tmpl.format(rec); }).join('');
+        todoList.innerHTML = rows.map(function (rec) { return tmpl.format(rec); }).join('');
         rowcount.innerHTML = store.query({'@type': 'todo', 'completed': 'false'}).length;
-        footer.classList[(store.query({'@type': 'todo'}).length == 0) ? 'add' : 'remove']('hidden');
+        footer.classList[(total == 0) ? 'add' : 'remove']('hidden');
+        footer.classList[(total == 0) ? 'add' : 'remove']('hidden');
     }
 
     // Make all entries state track 'toggle all' flag
@@ -37,6 +40,7 @@ __.onstart(function () {
     // Remove all records marked completed
     __.get('.clear-completed').on('click', function (ev) {
         store.query({'@type': 'todo', 'completed': 'true'}).forEach(store.remove.bind(store));
+        __.get('.toggle-all').checked = false;
     });
     // track routing - controls filtering
     window.addEventListener('hashchange', function () {
@@ -46,17 +50,17 @@ __.onstart(function () {
     });
 
     // catch change of complete status
-    main.delegate('change', 'input[type=checkbox]', function (ev) {
+    todoList.delegate('change', 'input[type=checkbox]', function (ev) {
         var _id = __(ev.target).parent('li').dataset.id;
         store.get(_id).completed = ev.target.checked;
     });
     // catch Click on "done" button
-    main.delegate('click', 'button.destroy', function (ev) {
+    todoList.delegate('click', 'button.destroy', function (ev) {
         var _id = __(ev.target).parent('li').dataset.id;
         store.remove(_id);
     });
     // catch Double Click on todo labels
-    main.delegate('dblclick', 'label', function (ev) {
+    todoList.delegate('dblclick', 'label', function (ev) {
         store.query({'@type': 'todo'}).forEach(function (rec) {
             rec.editing = 'false';
         });
@@ -64,7 +68,7 @@ __.onstart(function () {
         store.get(_id).editing = 'true';
     });
     // catch Enter in edit inputs
-    main.delegate('keyup', '.edit', function (ev) {
+    todoList.delegate('keyup', '.edit', function (ev) {
         if(ev.code !== 'Enter') return;
         var _id = __(ev.target).parent('li').dataset.id;
         var rec = store.get(_id);
@@ -74,8 +78,11 @@ __.onstart(function () {
     // catch Enter in new todo input
     input.delegate('keyup', '.new-todo', function (ev) {
         if(ev.code !== 'Enter') return;
-        var _id = new Date().valueOf().toString();
-        store.add(_id, {"@type": "todo", message: input.value, completed: false});
+        var value = input.value.trim();
+        if(value) {
+            var _id = new Date().valueOf().toString();
+            store.add(_id, {"@type": "todo", message: input.value, completed: false});
+        }
         input.value = '';
     });
 
